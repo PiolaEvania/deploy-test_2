7d2f7ce3db62dc3ca5478e9d30c4fa9bf40882d5
PiolaEvania <piolaevania@gmail.com>
2024-12-19 16:37:22 +0800

    Mengupdate beberapa code

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: 'https://route-sure.netlify.app',
+  origin: 'https://app-route-sure.vercel.app',
   credentials: true
 }));
 app.use(helmet());
