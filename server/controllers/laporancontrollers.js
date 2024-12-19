eda46f1accb478945b9a85c5ec1877edd12b4e84
PiolaEvania <piolaevania@gmail.com>
2024-12-19 23:53:06 +0800

    Mengupdate beberapa code

--- server/controllers/laporancontrollers.js
+++ server/controllers/laporancontrollers.js
@@ -1,6 +1,14 @@
 const Laporan = require('../models/Laporan');
 const cloudinary = require('cloudinary').v2;
 const streamifier = require('streamifier');
+const dotenv = require('dotenv');
+dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 exports.getLaporan = async (req, res) => {
   try {
